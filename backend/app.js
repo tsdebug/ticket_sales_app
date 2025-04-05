4b7b12b8b1595ac8e17baafd0e5f7d8d7095c3b6
dona125 <dona125@users.noreply.github.com>
2025-04-05 17:38:10 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const ticketRoutes = require("./routes/tickets");
 const authRoutes = require("./routes/auth");
+const insightRoutes = require('./routes/insights');
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/auth", authRoutes);
+app.use('/api/insights', insightRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
@@ -20,3 +22,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const eventRoutes = require('./routes/eventRoutes');
+app.use('/api', eventRoutes);
